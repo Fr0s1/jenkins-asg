6bc667fec5aceaecbfd86ef8fd8652a926dd4c16
Fr0s1 <dthieu223@gmail.com>
2022-12-27 15:58:40 +0700

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,11 @@ const os = require("os");
 const hostname = os.hostname();
 
 app.get("/", (req, res) => {
-  res.send(`Hello World  from ${hostname}!`);
+  res.send(`Hello World version 2 from ${hostname}!`);
+});
+
+app.get("/test", (req, res) => {
+  res.send(`New path version 2 from ${hostname}!`);
 });
 
 app.listen(port, () => {
