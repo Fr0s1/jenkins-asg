872a9af3056178fb1561bbefa8c520c853c18806
Fr0s1 <dthieu223@gmail.com>
2022-12-27 08:25:31 +0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+const os = require("os");
+const hostname = os.hostname();
+
+app.get("/", (req, res) => {
+  res.send(`Hello World  from ${hostname}!`);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
