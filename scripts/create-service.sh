4abc92f3cba20a34906da8dc259ab44147a73e16
Fr0s1 <dthieu223@gmail.com>
2022-12-27 15:41:29 +0700

    Edit create-service

--- scripts/create-service.sh
+++ scripts/create-service.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+NODE_EXECUTABLE_PATH=$(which node)
 cat << EOF > /etc/systemd/system/node.service
 [Unit]
 Description="Sample Express Application"
@@ -7,7 +8,7 @@ Description="Sample Express Application"
 [Service]
 User=root
 WorkingDirectory=/app
-ExecStart=node out.js
+ExecStart=${NODE_EXECUTABLE_PATH} out.js
 Restart=always
 
 [Install]
